99d501c0953904756425c39e882cc31c637f42bb
[vinald] <okiror1vinald@gmail.com>
2026-06-06 02:52:23 +0300

    create form

--- src/component/smoothieCard.js
+++ src/component/smoothieCard.js
@@ -2,7 +2,7 @@ const SmoothieCard = ({ smoothie }) => {
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
-      <p>{smoothie.methods}</p>
+      <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
     </div>
   );
