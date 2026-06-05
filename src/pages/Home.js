61f76429557b3709af701abca63877d346a3fe34
[vinald] <okiror1vinald@gmail.com>
2026-06-06 02:38:07 +0300

    smoothie card and db data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,42 @@
-import supabase from "../config/supabaseClient"
-import { useEffect, useState } from "react"
+import supabase from "../config/supabaseClient";
+import { useEffect, useState } from "react";
+import SmoothieCard from "../component/smoothieCard";
 
 const Home = () => {
-  const [fetchError, setFetchError] = useState(null)
-  const [data, setData] = useState(null)
+  const [fetchError, setFetchError] = useState(null);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data, error } = await supabase
-      .from("sb-smoothie")
-      .select()
+      const { data, error } = await supabase.from("smoothie").select();
+      console.log("Supabase response:", { data, error });
       if (error) {
-        setFetchError(error.message)
-        console.error("Error fetching data:", error)
-        setData(null)
+        setFetchError(error.message);
+        console.error("Error fetching data:", error);
+        setData(null);
       } else {
-        setData(data)
-        setFetchError(null)
+        setData(data);
+        setFetchError(null);
       }
-    }
-    fetchData()
-  }, [])
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="page home">
       <h2>Home</h2>
       {fetchError && <p>Error: {fetchError}</p>}
       {data && (
-        <ul>
-          {data.map(item => (
-            <li key={item.id}>{item.title} {item.rating} {item.methods}</li>
-          ))}
-        </ul>
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {data.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
