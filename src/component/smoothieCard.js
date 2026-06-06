3112e4341ae45b9f04feb79afaa77321abeab6e8
[vinald] <okiror1vinald@gmail.com>
2026-06-06 03:16:28 +0300

    making the whole card clickable

--- src/component/smoothieCard.js
+++ src/component/smoothieCard.js
@@ -2,17 +2,11 @@ import { Link } from 'react-router-dom'
 
 const SmoothieCard = ({ smoothie }) => {
   return (
-    <div className="smoothie-card">
+    <Link to={"/" + smoothie.id} className="smoothie-card">
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
-      <div className="buttons">
-        <Link to={"/" + smoothie.id} className="btn">
-          <i className="material-icons">edit</i>
-          Edit
-        </Link>
-      </div>
-    </div>
+    </Link>
   )
 }
 
